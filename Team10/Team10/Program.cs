739977d59ad752f35b58cbeffdbc5be472818d7c
lucashphan <99146431+lucashphan@users.noreply.github.com>
2022-02-08 14:08:20 -0600

    Initial codes
    Starting on feature 1

--- Team10/Team10/Program.cs
+++ Team10/Team10/Program.cs
@@ -1,6 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("This is a test");
-Console.WriteLine("Test number Two");
+﻿using System;
 
-Console.WriteLine("Eric made a commit!");
+namespace team10
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to SWEN5236 - Group 10 GRE Math project. \nThis " +
+                "program's purpose is to teach 6th grade math while using the GRE algorithm.");
+            Console.WriteLine("\nTo start, please select one of the following math problem options: (Enter 'q' to quit)");
+            Console.WriteLine("A.Multiplication \nB.Division \nC.Mixed Operations");
+            for (string input; (input = Console.ReadLine().ToUpper()) != "Q";) ;
+        }        
+           
+        static class multiplication
+        {
+
+        } 
+
+
+    }
+}
+
