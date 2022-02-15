91c8b8da00ef38378ef9a289926cf6cfb74d852e
lucashphan <99146431+lucashphan@users.noreply.github.com>
2022-02-14 19:27:08 -0600

    v2.01

--- Team10/Team10/Program.cs
+++ Team10/Team10/Program.cs
@@ -15,6 +15,7 @@ namespace team10
                 if (input == "A")
                 {
                     Console.WriteLine("You've selected Muliplication");
+                    Console.WriteLine();
                 }
                 else if (input == "B")
                 {
@@ -29,40 +30,30 @@ namespace team10
                     Console.WriteLine("Please select A, B, C, or Q to quit");
                 }
             }
-           
-        }
 
-        Tuple<string, int, string>[] multiplicationProblems =
-        {
-            new Tuple<string, int, string>("6 x 5", 30, "Hard"),
-            new Tuple<string, int, string>("1 x 1", 1, "Easy"),
-            new Tuple<string, int, string>("2 x 2", 4, "Medium")
-        };
 
-        Tuple<string, int, string>[] divisionProblems =
-        {
-            new Tuple<string, int, string>("30 / 3", 10, "Hard"),
-            new Tuple<string, int, string>("2 / 2", 1, "Easy"),
-            new Tuple<string, int, string>("10 / 5", 2, "Medium")
-        };
+            Tuple<string, int, string>[] multiplicationProblems =
+            {
+                new Tuple<string, int, string>("6 x 5", 30, "Hard"),
+                new Tuple<string, int, string>("1 x 1", 1, "Easy"),
+                new Tuple<string, int, string>("2 x 2", 4, "Medium")
+            };
 
-        Tuple<string, int, string>[] mixedProblems =
-        {
-            new Tuple<string, int, string>("6 x 5 / 2", 15, "Hard"),
-            new Tuple<string, int, string>("3 x 1 / 1", 3, "Easy"),
-            new Tuple<string, int, string>("4 x 2 / 4", 2, "Medium")
-        };
-        private static multiplication()
-        {
-            
-        }
-        private static division()
-        {
+            Tuple<string, int, string>[] divisionProblems =
+            {
+                new Tuple<string, int, string>("30 / 3", 10, "Hard"),
+                new Tuple<string, int, string>("2 / 2", 1, "Easy"),
+                new Tuple<string, int, string>("10 / 5", 2, "Medium")
+            };
 
-        }
-        private static mixedOperation()
-        {
+            Tuple<string, int, string>[] mixedProblems =
+            {
+                new Tuple<string, int, string>("6 x 5 / 2", 15, "Hard"),
+                new Tuple<string, int, string>("3 x 1 / 1", 3, "Easy"),
+                new Tuple<string, int, string>("4 x 2 / 4", 2, "Medium")
+            };
 
+            int score = 0;            
         }
     }
 }
